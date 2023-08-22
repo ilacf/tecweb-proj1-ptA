f20105a7cf510c3232b7cc6d812d47ea0375f9bf
ilacf <103431079+ilacf@users.noreply.github.com>
2023-08-22 10:40:14 -0300

    a

--- database.py
+++ database.py
@@ -1,6 +1,21 @@
 import sqlite3
+from dataclasses import dataclass
+
+@dataclass
+class Note:
+    id: int=None
+    title: str=None
+    content: str=''
+
 class Database():
     def __init__(self, banco):
         self.banco = banco
-        self.conn = sqlite3.connect(banco)
-    
+        self.conn = sqlite3.connect(banco+".db")
+        self.cur = self.conn.cursor()
+
+        self.cur.execute("CREATE TABLE IF NOT EXISTS note (id INT PRIMARY KEY, title STRING, content STRING NOT NULL);")
+
+    def add(self, note):
+        comando = f'INSERT INTO {self.banco} (title, content) VALUES (\'{note.title}\', \'{note.content}\');'
+        print(f'\n\n{comando}\n\n')
+        self.cur.execute(comando)
